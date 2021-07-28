dc5685ef01da1841fa81b88454ae590d01ba7283
Sophie Hoffman <33765268+shoffm@users.noreply.github.com>
2021-07-28 13:48:57 -0400

    Update fast5_sub_3.sh

--- fast5_sub_3.sh
+++ fast5_sub_3.sh
@@ -4,22 +4,14 @@
 module load ont-fast5-api
 
 #input path
-OUTPATH_SUB=/data/hoffmansf/read_isolation/fast5_subset_output
-READ_ID_LIST=/home/hoffmansf/read_isolation/read_IDs.txt
+OUTPATH_SUB=/path/to/fast5_subset_output
+READ_ID_LIST=/path/to/read_IDs.txt
 
-#cat /data/hoffmansf/read_isolation/fast5_paths_2_lin.txt | while read line
-cat /data/hoffmansf/read_isolation/fast5_paths_4_lin.txt | while read line
+cat /path/to/fast5_paths.txt | while read line
 do
-#echo $line
-#NAME=$(echo $line | awk -F'/' ' { print $NF } ')
 NAME=$(echo "${line//\//.}")
 NAME2="${NAME:1}"
-#echo ${NAME2}
-#ID=${$line | awk -F'/' '{ print $NF } '}
 OUTPATH=${OUTPATH_SUB}/${NAME2}/
-#echo ${OUTPATH}
-#INPATH=${line%/*}
-#echo $line
 fast5_subset --input ${line} --save_path ${OUTPATH} --read_id_list ${READ_ID_LIST}
 done
 
